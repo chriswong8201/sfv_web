200f45c73ef6bb3974bc36817bbb4f6cb50db475
YC Wong - IMT <MISYCW@voguelaundry.com>
2018-03-20 16:12:30 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));
+registerServiceWorker();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
